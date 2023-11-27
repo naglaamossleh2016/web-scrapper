1846fe96d596aa284b0f573d3a35b996b9b1b075
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2023-11-28 00:55:45 +0200

    addng python code to scrap data

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,1 +1,9 @@
 #web scapper code
+import requests
+from bs4 import BeautifulSoup
+
+response=requests.get('https://books.toscrape.com/')
+
+value=BeautifulSoup(response.content,"html.parser")
+books=value.find_all("article")
+print(books)
