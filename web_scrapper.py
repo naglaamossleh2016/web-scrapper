6a5fe49127bf8a4108271f3b72ed35ec6bd1be24
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2023-11-28 19:41:02 +0200

    comment line 9

--- web_scrapper.py
+++ web_scrapper.py
@@ -6,7 +6,7 @@ response=requests.get('https://books.toscrape.com/')
 
 value=BeautifulSoup(response.content,"html.parser")
 books=value.find_all("article")
-print(books[0])
+# print(books[0])
 for book in books:
     title = book.h3.a["title"]
     rating = book.p["class"][1]
