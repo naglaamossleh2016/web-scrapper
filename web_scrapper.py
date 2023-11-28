14d2d622c58d2737835bc867bd64316d6017ef72
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2023-11-28 19:38:02 +0200

    scrap book title rating and price

--- web_scrapper.py
+++ web_scrapper.py
@@ -6,4 +6,9 @@ response=requests.get('https://books.toscrape.com/')
 
 value=BeautifulSoup(response.content,"html.parser")
 books=value.find_all("article")
-print(books)
+print(books[0])
+for book in books:
+    title = book.h3.a["title"]
+    rating = book.p["class"][1]
+    price = book.select_one("p.price_color").get_text(strip=True)
+    print(f"Book title: {title} ***** rating: ({rating}) stars $$$$$ price: {price}")
